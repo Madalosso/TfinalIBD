faee1705e20cad9e0c426856cb896cb8f7bd69c8
madalosso <otaviomadalosso@gmail.com>
2015-05-31 15:04:52 -0300

    comentarios e espacos em branco resolvidos

--- tfibd.py
+++ tfibd.py
@@ -1,36 +1,67 @@
 import sys
 
 keyWords = ["SELECT","FROM","WHERE","UNION"]
-sqlReturn = ""
-campos
+campos=[]
 
 def getResultFromSelect(string,cont):
+	returnSQL = ""
 	termos = string.split(" ")
 
+	#remove espacos em branco da query
+	n = len(termos)
+	i=0
+	while i<len(termos):
+		if termos[i]=="":
+			termos.pop(i)
+			i-=1
+		i+=1
+	print termos
+	
+#verifica existencia da clausula SELECT
 	if termos[0]=="SELECT":
 
+#lista os indices dos campos a serem recuperados
+		campos = termos[1].split(',')
 
-		if string.find("FROM")!=-1:
-			
+#verifica a existencia do FROM
+		if termos[2]=="FROM":
 
+#obtem nome do arquivo(tabela)
+			nomeTabela = termos[3]
+			with open(nomeTabela) as tabela1:
+#abre tabela e le arquivo, para cada linha, adiciona o indice desejado na string de retorno
+				dataTable1 = tabela1.readlines()
+				for line in dataTable1:
+					reg = line.split(',')
+					for nArgs in campos:
+						returnSQL+=reg[int(nArgs)-1]
+						returnSQL+=", "
+					returnSQL+="\n"
 		else:
 			print "Erro!\n A query %d nao possui o FROM da consulta" % cont
 	else:
 		print "Erro!\n A query %d nao possui o SELECT da consulta" % cont
-	return 0
+	return returnSQL
 
 def main(argv):
 	#CONSULTA COMPOSTA POR : SELECT * FROM * WHERE * UNION
-
+	sqlReturn = ""
 	queryFileName = argv[1]
+	#abre arquivo com consulta
 	with open(queryFileName) as q:
+		#le arquivo inteiro como uma unica string e separa as diferentes consultas
 		data=q.read().replace('\n', ' ')
 		queries = data.split("UNION")
+
+		#executa cada consulta separadamente e une os resulados
 		cont=0
 		for q in queries:
-			getResultFromSelect(q,cont)
+			print q
+			sqlReturn += getResultFromSelect(q,cont)
 			cont+=1
-		print queries
+
+		#retorno da consulta
+		print sqlReturn
 
 
 if __name__ == "__main__": main(sys.argv)
